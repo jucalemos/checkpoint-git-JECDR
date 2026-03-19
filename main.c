f32eb344fea8cb1963a8dd747e268d406308836c
carloshenrique11087@gmail.com <carloshenrique11087@gmail.com>
2026-03-18 22:49:21 -0300

    Formatacao do main e adicao do RM

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 int main() {
     printf("Rafael Marinucci Peres RM569729");
     printf("David dos Reis Cardoso RM568938");
-    printf("Carlos Henrique Santos Dias ");
+    printf("Carlos Henrique Santos Dias RM573334");
     return 0;
 } 
