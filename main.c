c6809eec4d25a3ba23df144d57d01a0b9401e0c8
vhnb <victorredin122@gmail.com>
2026-03-18 23:05:58 -0300

    feat: adiciona nome do aluno Victor Henrique Nogueira

--- main.c
+++ main.c
@@ -3,5 +3,6 @@ int main() {
     printf("Rafael Marinucci Peres RM569729");
     printf("David dos Reis Cardoso RM568938");
     printf("Carlos Henrique Santos Dias RM573334");
+    printf('Victor Henrique Nogueira');
     return 0;
 } 
