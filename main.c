5748d076c951a79a7d6bc2732d2039935eeb6488
vhnb <victorredin122@gmail.com>
2026-03-18 23:36:31 -0300

    style: Alteração na formatação de todos os nomes em exibição

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
-    printf("Rafael Marinucci Peres RM569729");
-    printf("David dos Reis Cardoso RM568938");
-    printf("Carlos Henrique Santos Dias RM573334");
+    printf("Rafael Marinucci Peres RM569729\n");
+    printf("David dos Reis Cardoso RM568938\n");
+    printf("Carlos Henrique Santos Dias RM573334\n");
     printf('Victor Henrique Nogueira RM570021\n');
-     printf('Erick Yu xiang Li RM569305\n');
+    printf('Erick Yu xiang Li RM569305\n');
     return 0;
 } 
